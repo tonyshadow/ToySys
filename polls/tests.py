79f46ab2958fa7a7468ec060f2fc526ab69a6c69
Yi Zhang <yizhangucla@gmail.com>
2017-10-03 14:39:55 -0400

    travis ci trial with some little changes

--- polls/tests.py
+++ polls/tests.py
@@ -125,3 +125,4 @@ class QuestionDetailViewTests(TestCase):
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
+        
