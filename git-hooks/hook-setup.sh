7aa76af6867a9441dbcc0972bc61a0759bd67d34
Shadow <shadow.lzd@gmail.com>
2017-10-11 13:04:41 -0400

    update hooks

--- git-hooks/hook-setup.sh
+++ git-hooks/hook-setup.sh
@@ -6,14 +6,13 @@
 # how to install:
 # 1. open your terminal (bash)
 # 2. cd to your local repo of this project
-# 3. run git pull origin master (you will find git-hooks dir in your local repo)
+# 3. git pull origin master (you will find git-hooks dir in your local repo)
 # 4. cd to git-hooks dir
-# 5. run ./hook-setup.sh
-# 6. (if permission denied, run chmod -x ./hook-setup.sh, then run ./hook-setup.sh again)
-# 7. because it uses symlink to keep update, you just need to run this scipt once
-# 8. now, you have a pre-commit hook in your local repo, which will run static analysis
+# 5. chmod +x hook-setup.sh (give run permission to this script)
+# 6. ./hook-setup.sh (run this script)
+# 7. now, you have a pre-commit hook in your local repo, which will run static analysis
 #    before each of your commit
-# 9. enjoy!
+# 8. enjoy!
 
 # get the dir of local hooks
 GITHOOKS_DIR=$(git rev-parse --show-toplevel)/.git/hooks
@@ -27,3 +26,6 @@ fi
 
 # symlink local pre-commit hook to repo actual hook
 ln -s -f $REPOHOOKS_DIR/pre-commit $GITHOOKS_DIR/pre-commit
+
+# give run permission to pre-commit script)
+chmod +x pre-commit
